bc109c2fcec6ec9672a3781a2f522e719e51e72c
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2022-09-14 09:26:13 +0200

    Update Silvan.sql

--- Silvan.sql
+++ Silvan.sql
@@ -242,12 +242,14 @@ SELECT TOP 2 item.ItemName, item.QtyOnHand FROM item ORDER BY item.QtyOnHand;
 
 -- OPG 40 - CREATE A SIMPLE VIEW WITH ITEM NAMES AND ITEM PRICES ONLY
 CREATE VIEW namesAndPrices AS SELECT item.ItemName, item.ItemPrice FROM item;
+
 SELECT * FROM namesAndPrices;
 
 -- OPG 40 - CREATE A VIEW THAT WILL DISPLAY INVOICE NUMBER AND CUSTOMER NAMES FOR al CUSTOMERS
 CREATE VIEW namesAndInvoices AS 
 	SELECT customer.CustName, invoice.InvNo FROM customer
 	JOIN invoice ON customer.CustNo = invoice.CustNo;
+
 SELECT * FROM namesAndInvoices ORDER BY CustName;
 
 -- OPG 41 - CREATE AN INDEX FILE TO SPEED UP A SEARCH BASED ON CUSTOMER'S NAME
